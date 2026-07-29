01a2489cbe982c21b598cb1bdadbe6ca75323629
sneha suryavanshi <sneha.suryavanshi5656@gmail.com>
2026-07-29 10:51:36 +0530

    Test CORS

--- backend/app.py
+++ backend/app.py
@@ -14,13 +14,7 @@ app = FastAPI(
 # Allow requests from the React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-        "https://chainshield-frontend.onrender.com"   
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
